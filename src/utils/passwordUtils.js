52ce9b0715add628d96540c42c4fd00b9163b588
Angel Magdaleno López <76121102+AngelLopezM@users.noreply.github.com>
2026-02-11 13:41:17 -0600

    Fix: Corregir encriptación de contraseña usando SHA-512

--- src/utils/passwordUtils.js
+++ src/utils/passwordUtils.js
@@ -4,10 +4,11 @@ const SECRET_KEY = import.meta.env.VITE_APP_SECRET_KEY;
 
 export const hashPassword = (password) => {
   try {
-    return CryptoJS.SHA256(password + SECRET_KEY).toString();
+    const hash = CryptoJS.HmacSHA512(password, SECRET_KEY);
+    
+    return hash.toString(CryptoJS.enc.Hex);
   } catch (error) {
     console.error("Error al hashear contraseña:", error);
     throw new Error("Error al hashear la contraseña");
   }
 };
-
